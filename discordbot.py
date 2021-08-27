869bbfe9a7aafdad41c981605ade0bfa57f154d9
mike <mikeyu@future.net.co>
2021-08-27 18:05:14 +0800

    retry

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,7 @@ DSTLanguage=os.environ['DST']
 
 client = discord.Client()
 
-async def get_quote():
+def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text);
     quote = json_data[0]["q"] + " -" +json_data[0]["a"]
