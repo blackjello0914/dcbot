804425f147a5be2956ea255710cc24195b1819e0
mike <mikeyu@future.net.co>
2021-08-27 19:06:41 +0800

    test2

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,8 @@
 import discord
 import googletrans
 import os
-# import json
-# import requests
+import json
+import requests
 
 from pprint import pprint
 # 輸入自己Bot的TOKEN碼
@@ -12,11 +12,11 @@ DSTLanguage=os.environ['DST']
 
 client = discord.Client()
 
-# def get_quote():
-#     response = requests.get("https://zenquotes.io/api/random")
-#     json_data = json.loads(response.text);
-#     quote = json_data[0]["q"] + " -" +json_data[0]["a"]
-#     return quote
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text);
+    quote = json_data[0]["q"] + " -" +json_data[0]["a"]
+    return quote
 
 # 起動時呼叫
 @client.event
@@ -58,9 +58,9 @@ async def on_message(message):
     if msg.startswith("!古戰") :
         await message.reply("火古9/8~9/15 風古11月初 <:guraseeyou:873967596582625321>")
         return
-    # if msg.startswith("!隨便講點啥") :
-    #     await message.reply(get_quote() + "<:guraseeyou:873967596582625321>")
-    #     return
+    if msg.startswith("!隨便講點啥") :
+        await message.reply(get_quote() + "<:guraseeyou:873967596582625321>")
+        return
 
 
     if client.user in message.mentions: # @判定
