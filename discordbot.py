69b4f85c67127054688b3e735f8d778a88547cb4
mike <mikeyu@future.net.co>
2021-08-28 02:40:18 +0800

    hotfix2 for random dice

--- discordbot.py
+++ discordbot.py
@@ -72,7 +72,7 @@ async def on_message(message):
         return
     if msg.startwith("!dice"):
         try:
-            await message.reply(random.randint(1,msg[6:0]))
+            await message.reply(random.randint(1,int(msg[5:])))
         except:
             await message.reply("你妹在那邊亂填")
         return
