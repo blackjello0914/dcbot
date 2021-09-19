85ace1bafc270227ef14ad11cee400f7b5b6a572
blackjello <mikeyou0914@gmail.com>
2021-09-19 20:58:29 +0800

    fix4

--- discordbot.py
+++ discordbot.py
@@ -109,7 +109,7 @@ async def on_message(message):
             await message.reply("哪個北七在戳，484杯杯 <:guraseeyou:873967596582625321>")
             return
     if "拆丼" in msg or "拆井" in msg :
-        await message.reply(file=discord.File('./chai.png'))
+        await channel.send(file=discord.File('./chai.png'))
         return
     if "四一" in msg or "4187" in msg :
         await message.reply("4187 <:guraseeyou:873967596582625321>")
