075313dc6600cb2fffd90c4058782ecbbe7b0495
blackjello <mikeyou0914@gmail.com>
2022-03-04 13:36:05 +0800

    test new func

--- discordbot.py
+++ discordbot.py
@@ -14,7 +14,7 @@ TOKEN = os.environ['TOKEN']
 SRCLanguage=os.environ['SRC']
 DSTLanguage=os.environ['DST']
 
-DATA_PATH = os.path.join("data", "SML-Cogs", "togglerole")
+DATA_PATH = os.path.join("data")
 SETTINGS_JSON = os.path.join(DATA_PATH, "settings.json")
 
 client = discord.Client()
@@ -173,11 +173,86 @@ async def on_message(message):
     if msg.startswith("!testbot") :
         await message.reply(str(message))
         return
+class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-# 收到反應時呼叫
-@client.event
-async def on_reaction_add(self, reaction: discord.Reaction, user):
-    await reaction.message.channel.send(str(reaction))
-    await reaction.message.channel.send(str(user))
+        self.role_message_id = 949168167048060939 # ID of the message that can be reacted to to add/remove a role.
+        self.emoji_to_role = {
+            discord.PartialEmoji(name='🔴'): 0, # ID of the role associated with unicode emoji '🔴'.
+            discord.PartialEmoji(name='🟡'): 949151654970802187, # ID of the role associated with unicode emoji '🟡'.
+            discord.PartialEmoji(name='green', id=949177753461219328): 949151583051087923, # ID of the role associated with a partial emoji's ID.
+        }
+
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        """Gives a role based on a reaction emoji."""
+        # Make sure that the message the user is reacting to is the one we care about.
+        if payload.message_id != self.role_message_id:
+            return
+
+        guild = self.get_guild(payload.guild_id)
+        if guild is None:
+            # Check if we're still in the guild and it's cached.
+            return
+
+        try:
+            role_id = self.emoji_to_role[payload.emoji]
+        except KeyError:
+            # If the emoji isn't the one we care about then exit as well.
+            return
+
+        role = guild.get_role(role_id)
+        if role is None:
+            # Make sure the role still exists and is valid.
+            return
+
+        try:
+            # Finally, add the role.
+            await payload.member.add_roles(role)
+        except discord.HTTPException:
+            # If we want to do something in case of errors we'd do it here.
+            pass
+
+    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+        """Removes a role based on a reaction emoji."""
+        # Make sure that the message the user is reacting to is the one we care about.
+        if payload.message_id != self.role_message_id:
+            return
+
+        guild = self.get_guild(payload.guild_id)
+        if guild is None:
+            # Check if we're still in the guild and it's cached.
+            return
+
+        try:
+            role_id = self.emoji_to_role[payload.emoji]
+        except KeyError:
+            # If the emoji isn't the one we care about then exit as well.
+            return
+
+        role = guild.get_role(role_id)
+        if role is None:
+            # Make sure the role still exists and is valid.
+            return
+
+        # The payload for `on_raw_reaction_remove` does not provide `.member`
+        # so we must get the member ourselves from the payload's `.user_id`.
+        member = guild.get_member(payload.user_id)
+        if member is None:
+            # Make sure the member still exists and is valid.
+            return
+
+        try:
+            # Finally, remove the role.
+            await member.remove_roles(role)
+        except discord.HTTPException:
+            # If we want to do something in case of errors we'd do it here.
+            pass
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = MyClient(intents=intents)
+
 # Bot起動
 client.run(TOKEN)
